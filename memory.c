cc40997d604dcab6986607d20aa087060ad9fea5
Patience <patience.daniel.kindai@gmail.com>
2023-05-23 03:44:39 -0700

    Updated

--- memory.c
+++ memory.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -11,19 +11,19 @@ nclude <stdio.h>
 #include "shell.h"
 
 /**
- *  * bfree - Frees a pointer and NULLs the address.
- *   * @ptr: Address of the pointer to free.
- *    *
- *     * Return: 1 if freed, otherwise 0.
- *      */
+ * bfree - Frees a pointer and sets the address to NULL.
+ * @ptr: Address of the pointer to free.
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
 int bfree(void **ptr)
 {
-		if (ptr && *ptr)
-				{
-							free(*ptr);
-									*ptr = NULL;
-											return (1);
-												}
-			return (0);
+	if (ptr != NULL && *ptr != NULL)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
 }
 
